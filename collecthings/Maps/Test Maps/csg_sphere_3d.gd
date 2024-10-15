76dfb92a7fe7836f7a4e3db5d003f7748452b079
borokmate <borokmate@gmail.com>
2024-10-15 11:50:26 +0200

    Revert "PEAK"
    This reverts commit 56f70fadd061111b910c7149a351df4c10d32b3c.

--- collecthings/Maps/Test Maps/csg_sphere_3d.gd
+++ collecthings/Maps/Test Maps/csg_sphere_3d.gd
@@ -1,5 +1,4 @@
 extends CSGSphere3D
-@onready var player: Player = %Player
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,7 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(player.position, position)
-	position = position.move_toward(player.position, delta*6)
-	
+	rotation.y += 0.05
 	

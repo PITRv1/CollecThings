a8e3e47d7451308efa030727d88d9fed2d456b3f
scynthi <scynthisup@gmail.com>
2024-10-23 12:27:39 +0200

    Update ui.gd

--- Assets/UI/ui.gd
+++ Assets/UI/ui.gd
@@ -2,14 +2,13 @@ extends Control
 
 @onready var poslabel: Label = $HBoxContainer/VBoxContainer/pos
 @onready var speedlabel: Label = $HBoxContainer/VBoxContainer/speed
-var player = Global.player
+@onready var player: CharacterBody3D = get_tree().current_scene.get_node("Player")
 
 func _process(delta) -> void:
-	print(player.position)
 	var posx: String = " x: "+str(int(player.position.x))
 	var posy: String = " y: "+str(int(player.position.y))
 	var posz: String = " z: "+str(int(player.position.z))
 	poslabel.text = "Pos:"+posx+posy+posz
 	
-	speedlabel.text = "Speed:"+str(int(player.velocity))
+	speedlabel.text = "Speed:"+str(int(player.velocity.z))
 	

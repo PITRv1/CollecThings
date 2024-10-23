c1513a221e878ba691d6c57565c88ee68822e2b2
scynthi <scynthisup@gmail.com>
2024-10-23 19:03:37 +0200

    updated tablet

--- Assets/UI/ui.gd
+++ Assets/UI/ui.gd
@@ -1,17 +1,19 @@
 extends Control
 
-@onready var poslabel: Label = $HBoxContainer/VBoxContainer/pos
-@onready var speedlabel: Label = $HBoxContainer/VBoxContainer/speed
+@onready var poslabel: Label = $HBoxContainer/VBoxContainer/VBoxContainer2/pos
+@onready var speedlabel: Label = $HBoxContainer/VBoxContainer/VBoxContainer2/speed
 @onready var player: Player = get_tree().current_scene.get_node("Player")
-
+@onready var timer: Timer = $Timer
 func _ready() -> void:
 	print(player)
 
 func _process(delta) -> void:
-	var posx: String = " x: "+str(int(player.position.x))
-	var posy: String = " y: "+str(int(player.position.y))
-	var posz: String = " z: "+str(int(player.position.z))
+	var posx: String = " x:"+str(int(player.position.x))
+	var posy: String = " y:"+str(int(player.position.y))
+	var posz: String = " z:"+str(int(player.position.z))
 	poslabel.text = "Pos:"+posx+posy+posz
 	
+
+func _on_timer_timeout():
 	speedlabel.text = "Speed:"+str(int(player.velocity.length()))
-	
+	timer.start()

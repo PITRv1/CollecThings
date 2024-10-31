4e56d274979090b0e7660fc4d0060ccd1dd2e3f7
borokmate <borokmate@gmail.com>
2024-10-31 15:03:41 +0100

    ultraStates

---
+++ Entities/Enemy/among_us/state_machine.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var current_state : State
+var states: Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+			child.Transitioned.connect(on_child_transition)
+			
+func _process(delta: float):
+	if current_state:
+		current_state.Update(delta)
+		
+func _physics_process(delta: float):
+	if current_state:
+		current_state.Physics_Update(delta)
+
+func on_child_transition(state, new_state_name):
+	if state != current_state:
+		return
+		
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		return

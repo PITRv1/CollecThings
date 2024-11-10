bd79315d606eaca5294359928bddd39c8f6409d8
PITRv1 <petersandor.toth@gmail.com>
2024-11-10 11:39:29 +0100

    Merge branch 'enemyTest'

---
+++ Entities/Enemy/among_us/States/States.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name States
+
+signal Transitioned
+
+func Enter(previous_state):
+	pass
+	
+func Exit():
+	pass
+	
+func Update(_delta: float):
+	pass
+	
+func Physics_Update(_delta: float):
+	pass

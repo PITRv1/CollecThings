ff29949dbe4b172a354752398094777026c2b3a8
PITRv1 <petersandor.toth@gmail.com>
2024-11-18 15:40:14 +0100

    Revert "Pause menu is tewaking on linux"
    This reverts commit 018a4184cccb15841773c8969dbbca2725637531.

--- Assets/UI/Tablets/Pause Menu/pause_buttons_2d.gd
+++ Assets/UI/Tablets/Pause Menu/pause_buttons_2d.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var pause_menu : Node3D = get_tree().get_first_node_in_group("Pause_menu")
-@export var main_menu_scene : PackedScene
+var main_menu : PackedScene = preload("res://Maps/Test Maps/Main menu/Main_menu.tscn") 
 
 func _on_resume_pressed() -> void:
 	pause_menu.resume()
@@ -11,17 +11,15 @@ func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_reload_pressed() -> void:
-	get_tree().paused = false
-	get_tree().reload_current_scene()
+func _on_quit_pressed() -> void:
+	get_tree().quit()
 
 
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Maps/Main menu/Main_menu.tscn")
-	print(main_menu_scene)
+	get_tree().change_scene_to_packed(main_menu)
 	
 
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_reload_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()

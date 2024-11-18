58fce63655cf63f956d394237d9eb933d08b2691
PITRv1 <petersandor.toth@gmail.com>
2024-11-18 11:04:31 +0100

    added dash cooldown

--- Entities/Player/scripts/PlayerStates/DashingPlayerState.gd
+++ Entities/Player/scripts/PlayerStates/DashingPlayerState.gd
@@ -2,21 +2,29 @@ class_name DashingPlayerState
 
 extends PlayerMovementState
 
+@export var dashes_left : int = 2
 @export var dash_speed : float = 25.0
 
-@onready var dash_timer: Timer = $DashTimer
 
 func enter(_previous_state)-> void:
+	print(player.dashes_left)
+	
+	if player.dashes_left < 1:
+		%DashCooldown.start()
+		transition.emit("IdlePlayerState")
+	
+	player.dashes_left -= 1
+
 	var dash_dir = -player.camera.global_transform.basis.z.normalized()
 	
 	player.velocity = Vector3.ZERO
 	player.velocity.z = dash_dir.z * dash_speed
 	player.velocity.x = dash_dir.x * dash_speed
 	
-	if dash_timer:
-		dash_timer.stop()
+	if %DashLengthTimer:
+		%DashLengthTimer.stop()
 
-	dash_timer.start()
+	%DashLengthTimer.start()
 
 
 #Pls dont fuck with this its 12:00 PM and I want to go to bed. just let me fix it later cause it feels shit
@@ -33,15 +41,16 @@ func update(_delta)->void:
 
 	
 func physics_update(delta):
-	#player.update_gravity(delta)
 	player.update_input(delta)
 	
 	player.update_velocity(delta)
-#
+
 	weapon.sway_weapon(delta, false)
 
 func exit():
 	player.velocity = Vector3.ZERO
+	
+
 
 func _on_dash_timer_timeout() -> void:
 	transition.emit("IdlePlayerState")

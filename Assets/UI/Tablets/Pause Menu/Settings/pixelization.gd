108fda90d65da76e5901fdc428426e14845368d8
scynthi <85178488+scynthi@users.noreply.github.com>
2024-12-15 18:56:50 +0100

    Completed settings for pixelization shader (sorry peter)

--- Assets/UI/Tablets/Pause Menu/Settings/pixelization.gd
+++ Assets/UI/Tablets/Pause Menu/Settings/pixelization.gd
@@ -1,17 +1,15 @@
 extends HBoxContainer
 
-var current_world_env = null
-
-func get_current_world_environment() -> WorldEnvironment:
-	var current_scene = get_tree().current_scene
-	if current_scene:
-		var world_env = current_scene.get_node_or_null("WorldEnvironment") as WorldEnvironment
-		if world_env:
-			return world_env
-	return null
+var pixelization_shader = null
 
 func _ready() -> void:
-	current_world_env = get_current_world_environment()
+	$SliderBox/HSlider.editable = true
+	pixelization_shader = Global.get_environment_shader_by_property("down_scaling")
+	
+	if pixelization_shader == null:
+		$SliderBox/HSlider.editable = false
+	
 
 func _on_h_slider_value_changed(value: float) -> void:
-	print(current_world_env.compositor.compositor_effects)
+	pixelization_shader.set("down_scaling", value)
+	
--- global.gd
+++ global.gd
@@ -12,9 +12,20 @@ func change_scene_to(scene_path: String) -> void:
 	if scene_path:
 		next_scene = scene_path
 		get_tree().change_scene_to_packed(loading_screen)
-		
+	
 
 func change_weapon_special_icon(image: CompressedTexture2D) -> void:
 	var file : Image = Image.load_from_file(image.resource_path)
 	var texture : ImageTexture = ImageTexture.create_from_image(file)
 	Global.weapon_speacial.texture = texture
+	
+
+
+func get_environment_shader_by_property(property: String) -> CompositorEffect:
+	var world_env = get_tree().current_scene.get_node_or_null("WorldEnvironment") as WorldEnvironment
+	if world_env:
+		for effect in world_env.compositor.compositor_effects:
+			if effect.get(property):
+				return effect
+	return null
+	

64f8f19da4a592e744b440c4f9509f3349073981
PITRv1 <petersandor.toth@gmail.com>
2025-01-02 15:58:04 +0100

    Cam fix

--- Maps/Tutorial Map/take_off_cam.gd
+++ Maps/Tutorial Map/take_off_cam.gd
@@ -4,4 +4,5 @@ extends Camera3D
 @onready var space_ship: Node3D = $"../Space_ship/Space_ship"
 
 func _process(delta: float) -> void:
-	self.look_at(space_ship.global_position, Vector3.UP)
+	if space_ship:
+		self.look_at(space_ship.global_position, Vector3.UP)

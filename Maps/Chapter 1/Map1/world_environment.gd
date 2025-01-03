cd2a222e224b3d899274e83a23f014d9e8aa039b
ABEL0206 <wagnerabel95@gmail.com>
2025-01-03 20:47:33 +0100

    Environments added

---
+++ Maps/Chapter 1/Map1/world_environment.gd
@@ -0,0 +1,13 @@
+extends WorldEnvironment
+
+@onready var world_environment = $"."
+
+var environment_hub := preload("res://Assets/Environments/map1_environment_orange_brown.tres")
+var environment_map1 := preload("res://Assets/Environments/map1_environment2.tres") 
+
+func _on_area_3d_body_entered(body):
+	world_environment.environment = environment_map1
+
+
+func _on_area_3d_body_exited(body):
+	world_environment.environment = environment_hub

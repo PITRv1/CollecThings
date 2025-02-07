15d37701a317dacf41aec56948421030a2de35b6
ABEL0206 <wagnerabel95@gmail.com>
2025-02-07 20:40:44 +0100

    Tower added

---
+++ Maps/Chapter 1/Map1/company_cinematic.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@onready var anim_player = $"../AnimationPlayer2"
+@onready var camera_3d = $"../Camera3D"
+
+var player_cam = Camera3D
+var current_cam = Camera3D
+
+
+func cam_switch():
+		current_cam = camera_3d
+		current_cam.current = true
+		
+func cam_switch2():
+		current_cam.current = false
+
+
+func _on_body_entered(body):
+	print("alma")
+	anim_player.play("zoom in and out")
